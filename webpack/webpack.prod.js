038f6d2adcf4ae9c708421423b2c5d3738addd5a
Dan Lynch <pyramation@gmail.com>
2019-03-06 20:48:33 -0800

    example with vendors

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 const {
   prod_Path,
@@ -12,10 +14,34 @@ const {
   selectedPreprocessor
 } = require('./loader');
 
+function getVendorEntries(basePath, dependencies) {
+  return dependencies.reduce((prev, dependency) => {
+    const depPath = path.join(basePath, 'node_modules', dependency);
+
+    const files = fs
+      .readdirSync(depPath)
+      .filter(file => {
+        return (
+          (path.extname(file) === '.js' || path.extname(file) === '.css') &&
+          file !== path.basename(basePath)
+        );
+      })
+      .map(file => path.join(dependency, file));
+
+    return [...prev, ...files];
+  }, []);
+}
+
+const packageJSON = path.join(__dirname, '/../package.json');
+console.log({packageJSON})
+const dependencies = Object.keys(JSON.parse(fs.readFileSync(packageJSON).toString()).dependencies);
+const vendors = [...dependencies, ...getVendorEntries(path.join(__dirname, '/../'), dependencies)];
+
 module.exports = {
-  entry: {
-    main: './' + src_Path + '/index.js'
-  },
+  // entry: {
+  //   main: './' + src_Path + '/index.js'
+  // },
+  entry: vendors,
   output: {
     path: path.resolve(__dirname, prod_Path),
     filename: '[name].[chunkhash].js'
@@ -62,6 +88,7 @@ module.exports = {
       template: './' + src_Path + '/index.html',
       filename: 'index.html'
     }),
-    new WebpackMd5Hash()
+    new WebpackMd5Hash(),
+    new ManifestPlugin()
   ]
 };
